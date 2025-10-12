babd2366744dda136d21198a0a38dc830349e630
BolatMukashev <ya.ne.angel.kimi@gmail.com>
2025-10-12 16:15:15 +0500

    деплой на сервер

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
 from dotenv import dotenv_values
+
 
 config = dotenv_values(".env")
 
 
-YDB_ENDPOINT = config.get("YDB_ENDPOINT")
-YDB_PATH = config.get("YDB_PATH")
-YDB_TOKEN = config.get("YDB_TOKEN")
+YDB_ENDPOINT = os.environ.get("YDB_ENDPOINT") or config.get("YDB_ENDPOINT")
+YDB_PATH = os.environ.get("YDB_PATH") or config.get("YDB_PATH")
+YDB_TOKEN = os.environ.get("YDB_TOKEN") or config.get("YDB_TOKEN")
